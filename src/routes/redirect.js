1cdff6d714fb830319167cae0ddd482c08dfd378
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-07-04 17:29:51 -0600

    feat: add cache to redirect route
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const RedirectService = require('../services/redirect');
-const { setClientCache } = require('../utils/cache');
+const { nodeCache, setClientCache } = require('../utils/cache');
 const { FIVE_MINUTES_IN_SECONDS } = require('../utils/timeConst');
 
 const redirectService = new RedirectService();
@@ -12,10 +12,18 @@ function redirectRouter(app) {
 
   router.get('/*', async function (req, res, next) {
     const path = req.params[0];
+    let url;
+
     try {
-      const redirectData = await redirectService.getByPath(path);
+      if (nodeCache.has(path)) {
+        url = nodeCache.get(path);
+      } else {
+        const redirectData = await redirectService.getByPath(path);
+        url = redirectData.url;
+        nodeCache.set(path, url, FIVE_MINUTES_IN_SECONDS);
+      }
       setClientCache(res, FIVE_MINUTES_IN_SECONDS);
-      res.redirect(redirectData.url);
+      res.redirect(url);
     } catch (err) {
       next(err);
     }
--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,11 +1,16 @@
 const config = require('../config');
+const NodeCache = require('node-cache');
 
-function setClientCache(res, seconds) {
+function setClientCache(res, ttl) {
   if (!config.dev) {
-    res.set('Cache-Control', `public, max-age=${seconds}`);
+    res.set('Cache-Control', `public, max-age=${ttl}`);
   }
 }
 
+/** @type {NodeCache} */
+const nodeCache = new NodeCache();
 
-
-module.exports = { setClientCache };
+module.exports = {
+  setClientCache,
+  nodeCache,
+};
